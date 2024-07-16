9902398469ff8c03ed7082caae00d6d80a1d9c21
Cenote <himangshukalita131@gmail.com>
2024-07-16 20:31:21 +0530

    Login api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -48,6 +48,33 @@ export const signup = async (req, res, next) => {
   }
 }
 
-export const login = (req, res) => {}
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+
+    const validUser = await User.findOne({ email })
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"))
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials"))
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+      _id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+      profilePic: validUser.profilePic,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 export const logout = (req, res) => {}
