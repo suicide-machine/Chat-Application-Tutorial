c956bab59897a2d4a4953f6016c9712a2579091a
Cenote <himangshukalita131@gmail.com>
2024-07-16 18:25:36 +0530

    User model

---
+++ backend/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
