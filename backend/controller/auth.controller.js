4656b6cc0d5a019a810271226058304a8203fc35
Cenote <himangshukalita131@gmail.com>
2024-07-16 20:35:57 +0530

    Logout functionality

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -77,4 +77,14 @@ export const login = async (req, res, next) => {
   }
 }
 
-export const logout = (req, res) => {}
+export const logout = (req, res) => {
+  try {
+    res.clearCookie("access_token")
+
+    res.status(200).json({
+      message: "User has been loggged out successfully!",
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,6 +7,6 @@ router.post("/signup", signup)
 
 router.post("/login", login)
 
-router.post("/logout", logout)
+router.get("/logout", logout)
 
 export default router
