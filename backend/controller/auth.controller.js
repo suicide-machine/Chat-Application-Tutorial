2bf8b2a86e8de4b243c2e2079235856f02e8289b
Cenote <himangshukalita131@gmail.com>
2024-07-16 20:22:12 +0530

    middleware to handle possible error

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import jwt from "jsonwebtoken"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password, confirmPassword, gender } = req.body
 
   let validUser
@@ -10,16 +11,11 @@ export const signup = async (req, res) => {
   validUser = await User.findOne({ email })
 
   if (validUser) {
-    return res.status(400).json({
-      success: false,
-      message: "User already exist",
-    })
+    return next(errorHandler(400, "User already exists"))
   }
 
   if (password !== confirmPassword) {
-    return res.status(400).json({
-      error: "Passwords don't match",
-    })
+    return next(errorHandler(400, "Password don't match"))
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -48,10 +44,7 @@ export const signup = async (req, res) => {
       profilePic: newUser.profilePic,
     })
   } catch (error) {
-    console.log("Error " + error)
-    res.status(500).json({
-      error: "Internal server error",
-    })
+    next(error)
   }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -33,3 +33,15 @@ app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT)
 })
+
+// error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Server Error"
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
