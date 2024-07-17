4ce45f139477c375dae4a5853f355c8b53236108
Cenote <himangshukalita131@gmail.com>
2024-07-17 13:48:09 +0530

    Signup page UI

---
+++ frontend/src/component/GenderCheckbox.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const GenderCheckbox = () => {
+  return (
+    <div className="flex">
+      <div className="form-control">
+        <label className="label gap-2 cursor-pointer">
+          <span className="label-text">Male</span>
+
+          <input type="checkbox" className="checkbox border-slate-900" />
+        </label>
+      </div>
+
+      <div className="form-control">
+        <label className="label gap-2 cursor-pointer">
+          <span className="label-text">Female</span>
+
+          <input type="checkbox" className="checkbox border-slate-900" />
+        </label>
+      </div>
+    </div>
+  )
+}
+
+export default GenderCheckbox
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,71 @@
 import React from "react"
+import GenderCheckbox from "../component/GenderCheckbox"
+import { Link } from "react-router-dom"
 
 const SignUp = () => {
-  return <div>SignUp</div>
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className=" w-full p-6 rounded-lg shadow-md bg-gray-200">
+        <h1 className="text-3xl font-semibold text-center text-slate-700">
+          Signup to
+          <span className="text-blue-700"> Chat Application</span>
+        </h1>
+
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Email</span>
+            </label>
+
+            <input
+              type="email"
+              placeholder="example@gmail.com"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+
+            <input
+              type="email"
+              placeholder="Enter Password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+
+          <GenderCheckbox />
+
+          <Link
+            to={"/login"}
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            Already have an account?
+          </Link>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
